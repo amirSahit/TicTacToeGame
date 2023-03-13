ae30020de8ac3d426f560e617bf89392b75a8a52
intertemp <63150529+intertemp@users.noreply.github.com>
2023-03-13 10:15:57 +0100

    created a grid (with html&css

--- tictactoe/src/main.ts
+++ tictactoe/src/main.ts
@@ -1,1 +1,14 @@
 import "./style.css";
+
+//create a grid with js
+let headerElement = document.querySelector("#header");
+console.log(headerElement);
+let div = document.createElement("div");
+headerElement?.append(div);
+headerElement?.append(div);
+headerElement?.append(div);
+headerElement?.append(div);
+headerElement?.append(div);
+headerElement?.append(div);
+headerElement?.append(div);
+headerElement?.append(div);
